55cc3be13934bf6ce68e3479f279e36213461957
adityafjri <adityafjri@gmail.com>
2024-02-14 23:57:22 +0700

    berhasil indegree

--- graph.js
+++ graph.js
@@ -2,13 +2,22 @@ const { Graph } = require('graphology');
 const bfs = require('graphology-traversal').bfs;
 const connectedComponents = require('graphology-components').connectedComponents;
 const stronglyConnectedComponents = require('graphology-components').stronglyConnectedComponents;
-const createPagerank = require('ngraph.pagerank');
+const createPagerank = require('graphology-pagerank');
 const fs = require('fs');
+
+class NotFoundGraphError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = 'NotFoundGraphError';
+  }
+}
 
 // Read JSON files
 const nodeData = JSON.parse(fs.readFileSync('./public/data/graphNode.json', 'utf8'));
 const edgeMengingatData = JSON.parse(fs.readFileSync('./public/data/graphEdgeMengingat.json', 'utf8'));
 const edgeMenimbangData = JSON.parse(fs.readFileSync('./public/data/graphEdgeMenimbang.json', 'utf8'));
+
+if (!nodeData) throw new NotFoundGraphError("Graph.inDegree: could not find the node data in the graph.");
 
 // Creating a graph
 const graphMengingat = new Graph();
@@ -26,24 +35,61 @@ function addOrUpdateNode(graph, node) {
   }
 }
 
+function addEdge(graph, edge) {
+    const { src, dst } = edge;
+
+    // Check if source and destination nodes exist
+    if (!graph.hasNode(src)) {
+        console.warn(`Warning: Source node "${src}" not found for edge from "${src}" to "${dst}". Skipping this edge.`);
+        return;  // Skip this edge
+    }
+
+    if (!graph.hasNode(dst)) {
+        console.warn(`Warning: Target node "${dst}" not found for edge from "${src}" to "${dst}". Skipping this edge.`);
+        return;  // Skip this edge
+    }
+
+    // Add the edge to the graph
+    graph.addEdge(src, dst);
+}
+
+
 // Adding or updating nodes
 nodeData.forEach(node => addOrUpdateNode(graphMengingat, node));
-edgeMengingatData.forEach(edge => graphMengingat.addEdge(edge.source, edge.target, edge));
+
+// Adding edges
+edgeMengingatData.forEach(edge => {
+    try {
+        addEdge(graphMengingat, edge);
+        console.log("Edge added from", edge.src, "to", edge.dst);
+    } catch (error) {
+        console.error(error.message);
+    }
+});
 
 // Displaying vertices and edges
-console.log("Vertices (graphMengingat):", graphMengingat.nodes().toArray());
-console.log("Edges (graphMengingat):", graphMengingat.edges().toArray());
+console.log("Vertices (graphMengingat) after adding nodes:", graphMengingat.nodes());
+console.log("Edges (graphMengingat) after adding edges:", graphMengingat.edges());
 
 // In-degree and Out-degree
-const inDegrees = graphMengingat.inDegree();
-console.log("In-Degrees:", inDegrees);
+const inDegrees = {};
+const outDegrees = {};
 
-const outDegrees = graphMengingat.outDegree();
+graphMengingat.nodes().forEach(node => {
+    if (graphMengingat.hasNode(node)) {
+        inDegrees[node] = graphMengingat.inDegree(node);
+        outDegrees[node] = graphMengingat.outDegree(node);
+    } else {
+        console.warn(`Warning: Node "${node}" not found in the graph. Skipping in-degree and out-degree calculations for this node.`);
+    }
+});
+
+console.log("In-Degrees:", inDegrees);
 console.log("Out-Degrees:", outDegrees);
 
 // PageRank
 const pageRank = createPagerank(graphMengingat);
-console.log("PageRank:", pageRank.rank());
+console.log("PageRank:", pageRank);
 
 // Connected Components
 const cc = connectedComponents(graphMengingat);
@@ -53,6 +99,9 @@ console.log("Connected Components:", cc);
 const scc = stronglyConnectedComponents(graphMengingat, { maxIterations: 10 });
 console.log("Strongly Connected Components:", scc);
 
-// Breadth-First Search
-const bfsResult = bfs(graphMengingat, { start: '1', goal: '2' });
-console.log("BFS Result:", bfsResult);
+// BFS
+const bfsCallback = (node, information) => {
+    console.log(node); 
+};
+
+bfs(graphMengingat, bfsCallback, { start: '34 tahun 2008', goal: '22 tahun 1999' });
